f7348932484f1c8f43d3372b8ab14e12b2ddd3f4
Simone Vendramini <s.vendramini1@campus.unimib.it>
2024-04-23 13:42:39 +0200

    import matrix

--- Main.jl
+++ Main.jl
@@ -1,4 +1,9 @@
 include("Utils.jl") 
 using .utils
 
-utils.import_matrix("./data/spa1.mtx")
+spa1 = "./data/spa1.mtx"
+sparse_matrix = @time utils.import_sparse_matrix(spa1)
+matrix = @time utils.import_matrix("./data/spa1.mtx")
+
+println(sizeof(sparse_matrix))
+println(sizeof(matrix))
--- Utils.jl
+++ Utils.jl
@@ -1,11 +1,46 @@
 module utils 
 
-function import_matrix(path)
-    open(path) do file
-        s = readline(file)
-        println(s)
+    using SparseArrays
+
+    function import_matrix(path)
+        open(path) do file
+            s = readline(file)
+            rows, cols, value = split(s, "  ")
+            rows = parse(Int32, rows)
+            cols = parse(Int32, cols)
+            value = parse(Int32, value)
+            
+            matrix = zeros(rows, cols)
+            for line in eachline(file)
+                i, j, v = split(line, "  ")
+                i = parse(Int32, i)
+                j = parse(Int32, j)
+                v = parse(Float64, v)
+                matrix[i, j] = v
+            end
+            return matrix
+        end
     end
-end
+
+    function import_sparse_matrix(path)
+        open(path) do file
+            s = readline(file)
+            rows, cols, value = split(s, "  ")
+            rows = parse(Int32, rows)
+            cols = parse(Int32, cols)
+            value = parse(Int32, value)
+            
+            matrix = spzeros(rows, cols)
+            for line in eachline(file)
+                i, j, v = split(line, "  ")
+                i = parse(Int32, i)
+                j = parse(Int32, j)
+                v = parse(Float64, v)
+                matrix[i, j] = v
+            end
+            return matrix
+        end
+    end
 
 
 end
