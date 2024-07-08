cc1656a8b1b76e2aa30d7cce34fc0d8ec5696b92
Emamasi-uni <e.masiero@campus.unimib.it>
2024-07-08 16:07:40 +0200

    fix dash

--- Main2_pt2.jl
+++ Main2_pt2.jl
@@ -1,42 +1,85 @@
-include("Utils.jl") 
+include("Utils.jl")
 include("MyLib.jl")
-
+using Dash, Base64, ImageView, Images
 using FFTW
 using Statistics
 using JSON
-using Images
-using ImageView
 
-image_path = "immagini/bridge.bmp"
+app = dash()
 
-img = load(image_path)
-img = Gray.(img)
-imshow(img)
-println(size(img))
+app.layout = html_div(
+    style = Dict("text-align" => "center", "font-family" => "Arial")
+) do
+    html_h2("Inserisci due valori interi e carica un'immagine:"),
+    html_div(
+        style = Dict("margin" => "auto", "width" => "50%"),
+        children = [
+            html_div(
+                style = Dict("margin-bottom" => "20px"),
+                children = [
+                    "Dimensione della finestra: ",
+                    dcc_input(id = "input-1", type = "number", style = Dict("margin-left" => "10px"))
+                ]
+            ),
+            html_div(
+                style = Dict("margin-bottom" => "20px"),
+                children = [
+                    "Taglio delle frequenze: ",
+                    dcc_input(id = "input-2", type = "number", style = Dict("margin-left" => "10px"))
+                ]
+            ),
+            html_div(
+                style = Dict("margin-bottom" => "20px"),
+                children = [
+                    "Carica immagine: ",
+                    dcc_upload(id = "upload-image", children = html_button("Carica File", style = Dict("margin-left" => "10px")))
+                ]
+            ),
+            html_button("Elabora Immagine", id="process-button", style = Dict("margin-top" => "20px")),
+            html_div(id = "output-message", style = Dict("margin-top" => "20px", "color" => "blue")),
+            html_div(id = "output-image-path", style = Dict("margin-top" => "20px"))
+        ]
+    )
+end
 
-f = 20
-d = 8
+callback!(app, Output("output-message", "children"),
+    Input("process-button", "n_clicks"),
+    State("upload-image", "contents"),
+    State("input-1", "value"),
+    State("input-2", "value")
+) do n_clicks, contents, f, d
+    if n_clicks != nothing && contents != nothing && f != nothing && d != nothing
+        try
+            # Decode and save the uploaded image
+            img_data = Base64.base64decode(split(contents, ",")[2])
+            temp_filename = "temp_image.bmp"
+            open(temp_filename, "w") do file
+                write(file, img_data)
+            end
+            
+            # Load and process the image
+            img = load(temp_filename)
+            img_gray = Gray.(img)
+            imshow(img_gray, name="Original Image")
+            
+            blocks = split_image_into_blocks(img_gray, f)
+            ff = image_compress(blocks, d)
+            height, width = size(img_gray)
+            height = Int(floor(height / f) * f)
+            width = Int(floor(width / f) * f)
+            ff_img = reassemble_from_blocks(ff, height, width, f)
+            ff_img = Gray{N0f8}.(ff_img)
+            imshow(ff_img, name="Compressed Image")
 
-blocks = split_image_into_blocks(img, f)
+            return "Immagine processata e visualizzata."
+            
+        catch e
+            println("Errore durante il processamento dell'immagine: $e")
+            return "Errore durante il processamento dell'immagine."
+        end
+    else
+        return "Valori mancanti o immagine non caricata."
+    end
+end
 
-# Visualizza il primo blocco per verificare
-# println(sqrt(size(blocks)[1]))
-
-# for i in 1:f*f
-#     println("\nSize: ", size(blocks[i]))
-# end
-
-# block = Float64.(Array(blocks[1]))
-# cc = FFTW.dct(block)
-# print(size(cc))
-
-ff = image_compress(blocks, d)
-
-height, width = size(img)
-height = Int(floor(height/f)*f)
-width = Int(floor(width/f)*f)
-ff_img = reassemble_from_blocks(ff, height, width, f)
-ff_img = Gray{N0f8}.(ff_img)
-
-println(size(ff_img))
-imshow(ff_img)
+run_server(app, "0.0.0.0", debug=true)
